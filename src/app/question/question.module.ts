8561d33057b0d8fba8f57958aa2856e19012b303
KrisiKostadinov <krisi.199898@gmail.com>
2020-02-27 16:14:40 +0200

    Created project

---
+++ src/app/question/question.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddQuestionComponent } from './add-question/add-question.component';
+
+
+
+@NgModule({
+  declarations: [
+    AddQuestionComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class QuestionModule { }
